d5525d53d56e58cd8983a6de755918db88fd0398
DaanV2 <daanverstraten@hotmail.com>
2020-05-24 15:07:47 +0200

    Updated Code

--- RockstarToDotNet-API/Language/LexicalAnalyzer/Classes/LexicalAnalyzer/LexicalAnalyzer.cs
+++ RockstarToDotNet-API/Language/LexicalAnalyzer/Classes/LexicalAnalyzer/LexicalAnalyzer.cs
@@ -8,6 +8,6 @@ using System.Threading.Tasks;
 namespace Rockstar.Language.LexicalAnalyzer {
     ///DOLATER <summary>Add description for class: LexicalAnalyzer</summary>
     public partial class LexicalAnalyzer {
-
+        
     }
 }
--- RockstarToDotNet-API/Language/LexicalAnalyzer/Classes/TokenIdentifier/TokenIdentifier.cs
+++ RockstarToDotNet-API/Language/LexicalAnalyzer/Classes/TokenIdentifier/TokenIdentifier.cs
@@ -8,6 +8,6 @@ using System.Threading.Tasks;
 namespace Rockstar.Language.LexicalAnalyzer {
     ///DOLATER <summary>Add description for class: TokenIdentifier</summary>
     public partial class TokenIdentifier {
-        //DOLATER Fill code file for TokenIdentifier
+        
     }
 }
